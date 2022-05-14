c48e7d62ea5e5e61a9c54eb33c53ec6b1120263e
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-05-14 08:58:19 -0400

    mapeando a entidade pessoa

--- aulajpamaven/src/main/java/entities/Person.java
+++ aulajpamaven/src/main/java/entities/Person.java
@@ -2,11 +2,27 @@ package entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_person")
 public class Person implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 120,unique = true, nullable = false)
 	private String name;
+	
+	@Column(unique = true, nullable = false)
 	private String email;
 	
 	public Person() {
