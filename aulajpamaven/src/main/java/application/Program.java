9ea1de35ce67e5b98922c09a59fd209106243dec
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-05-14 09:04:49 -0400

    salvando um obj no Bd

--- aulajpamaven/src/main/java/application/Program.java
+++ aulajpamaven/src/main/java/application/Program.java
@@ -1,18 +1,31 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import entities.Person;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Person p = new Person(1, "Jackson Almeida", "jackson.almeida@gmail.com");
-		Person p1 = new Person(2, "Jackson Rodrigues", "jackson.rodrigues@gmail.com");
-		Person p2 = new Person(3, "Jackson Silva", "jackson.silva@gmail.com");
+		Person p = new Person(null, "Jackson Almeida", "jackson.almeida@gmail.com");
+		Person p1 = new Person(null, "Jackson Rodrigues", "jackson.rodrigues@gmail.com");
+		Person p2 = new Person(null, "Jackson Silva", "jackson.silva@gmail.com");
 		
-		System.out.println(p);
-		System.out.println(p1);
-		System.out.println(p2);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		
+		EntityManager em = emf.createEntityManager();
+		
+		em.persist(p);
+		em.persist(p1);
+		em.persist(p2);
+		
+		em.getTransaction().begin();
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
 		
 	}
 
