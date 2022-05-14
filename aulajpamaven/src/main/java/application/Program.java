6527e959422dbd1cbda11b1d64a367271d1b197e
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-05-14 09:08:11 -0400

    Buscando por id um obj no Bd

--- aulajpamaven/src/main/java/application/Program.java
+++ aulajpamaven/src/main/java/application/Program.java
@@ -10,22 +10,16 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Person p = new Person(null, "Jackson Almeida", "jackson.almeida@gmail.com");
-		Person p1 = new Person(null, "Jackson Rodrigues", "jackson.rodrigues@gmail.com");
-		Person p2 = new Person(null, "Jackson Silva", "jackson.silva@gmail.com");
-		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
-		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");	
 		EntityManager em = emf.createEntityManager();
 		
-		em.persist(p);
-		em.persist(p1);
-		em.persist(p2);
+
+		Person p = em.find(Person.class, 1);
 		
-		em.getTransaction().begin();
-		em.getTransaction().commit();
+		System.out.println(p);
 		
-		System.out.println("Pronto!");
+		emf.close();
+		em.close();
 		
 	}
 
